484de95160415ad84f8ba66d6d7982009fc56381
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-06-07 20:40:42 +0700

    update test deploy with forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        URL::forceScheme('https');
+        URL::forceRootUrl(config('app.url'));
     }
 }
